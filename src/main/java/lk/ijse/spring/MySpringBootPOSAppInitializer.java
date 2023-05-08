714b443e141ec88cb717deed98739548bdc02c3e
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-08 14:34:05 +0530

    update project

---
+++ src/main/java/lk/ijse/spring/MySpringBootPOSAppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MySpringBootPOSAppInitializer {
+
+    public String PORT=System.getenv("PORT");
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MySpringBootPOSAppInitializer.class);
+        System.out.println("spring start ....!");
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
